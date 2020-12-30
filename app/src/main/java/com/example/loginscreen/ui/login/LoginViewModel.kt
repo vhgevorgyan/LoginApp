d93873f567dec58e5d30d71476cfb9c5b5aaa56e
vhgevorgyan <76774943+vhgevorgyan@users.noreply.github.com>
2020-12-30 12:18:53 +0100

    initial commit

---
+++ app/src/main/java/com/example/loginscreen/ui/login/LoginViewModel.kt
@@ -0,0 +1,67 @@
+package com.example.loginscreen.ui.login
+
+import androidx.core.util.PatternsCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.loginscreen.R
+import com.example.loginscreen.data.LoginRepository
+import com.example.loginscreen.data.Result
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
+
+    private val _loginForm = MutableLiveData<LoginFormState>()
+    val loginFormState: LiveData<LoginFormState> = _loginForm
+
+    private val _loginResult = MutableLiveData<LoginResult>()
+    val loginResult: LiveData<LoginResult> = _loginResult
+
+    fun login(username: String, password: String) {
+        // can be launched in a separate asynchronous job
+        val result = loginRepository.login(username, password)
+
+        if (result is Result.Success) {
+            _loginResult.value = LoginResult(success = LoggedInUserView(displayName = result.data.displayName))
+        } else {
+            _loginResult.value = LoginResult(error = R.string.login_failed)
+        }
+    }
+
+    fun loginDataChanged(username: String, password: String) {
+        if (!isUserNameValid(username)) {
+            _loginForm.value = LoginFormState(usernameError = R.string.invalid_username)
+        } else if (!isPasswordValid(password)) {
+            _loginForm.value = LoginFormState(passwordError = R.string.invalid_password)
+        } else {
+            _loginForm.value = LoginFormState(isDataValid = true)
+        }
+    }
+
+    private fun isUserNameValid(username: String): Boolean {
+        return if (username.contains('@')) {
+            PatternsCompat.EMAIL_ADDRESS.matcher(username).matches()
+        } else {
+            username.isNotBlank()
+        }
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        return if ( password.length >= 12){
+            isValidPassword(password)
+        } else{
+           false
+        }
+    }
+
+    fun isValidPassword(password: String?): Boolean {
+        val pattern: Pattern
+        val matcher: Matcher
+        val PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$"
+        pattern = Pattern.compile(PASSWORD_PATTERN)
+        matcher = pattern.matcher(password)
+        return matcher.matches()
+    }
+
+}
